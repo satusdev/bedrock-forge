9f5d30638f0830aaa326803b8785440ff7dd5696
nadbad <nadeem.develops@gmail.com>
2025-07-24 11:08:20 +0200

    refactor: change location of template folder

--- websites/template/www/web/app/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1744616090;s:7:"authKey";s:64:"jz?/PY)B5n?sBjdjBCDgeTm]UajN5>u%G`ms&ATrM- %;/8D]wg#/_>&5JN2vIlf";}
