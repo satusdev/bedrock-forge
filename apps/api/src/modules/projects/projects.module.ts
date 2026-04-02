304293e79993f89a0c46a67cd5e60e3a3f24cb70
nadbad <nadeem.develops@gmail.com>
2026-04-02 14:57:50 +0200

    feat(api): add full project create flow with CyberPanel provisioning and enriched project DTO

--- apps/api/src/modules/projects/projects.module.ts
+++ apps/api/src/modules/projects/projects.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { QUEUES } from '@bedrock-forge/shared';
 import { EncryptionModule } from '../../common/encryption/encryption.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { DomainsModule } from '../domains/domains.module';
 import { MonitorsModule } from '../monitors/monitors.module';
+import { CyberpanelModule } from '../cyberpanel/cyberpanel.module';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 import { ProjectsRepository } from './projects.repository';
@@ -12,8 +14,10 @@ import { ProjectsRepository } from './projects.repository';
 	imports: [
 		BullModule.registerQueue({ name: QUEUES.PROJECTS }),
 		EncryptionModule,
+		PrismaModule,
 		DomainsModule,
 		MonitorsModule,
+		CyberpanelModule,
 	],
 	controllers: [ProjectsController],
 	providers: [ProjectsRepository, ProjectsService],
