7e86d6c76c425b3dd9d7a7f05b3e1b8ac9ac1515
nadbad <n.alhassan@lamah.com>
2026-04-02 14:58:13 +0200

    feat(worker): extend create-bedrock processor with CyberPanel provisioning and environment clone support

--- apps/worker/src/processors/create-bedrock/create-bedrock-processor.module.ts
+++ apps/worker/src/processors/create-bedrock/create-bedrock-processor.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { QUEUES } from '@bedrock-forge/shared';
 import { CreateBedrockProcessor } from './create-bedrock.processor';
+import { EncryptionModule } from '../../encryption/encryption.module';
 
 @Module({
-	imports: [BullModule.registerQueue({ name: QUEUES.PROJECTS })],
+	imports: [
+		BullModule.registerQueue({ name: QUEUES.PROJECTS }),
+		EncryptionModule,
+	],
 	providers: [CreateBedrockProcessor],
 })
 export class CreateBedrockProcessorModule {}
